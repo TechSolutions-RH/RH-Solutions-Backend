f5df0c725fee53f741fcac873b92cca201588fc6
Gabrielfers <gabrielfer.s123@gmail.com>
2025-07-18 10:28:39 -0300

    refactor: remover CPF do payload do token JWT

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -17,7 +17,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     return {
       id: payload.sub,
       email: payload.email,
-      cpf: payload.cpf,
       role: payload.role,
     };
   }
